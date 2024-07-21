8002f5109a97e18396fdb05262527c6cd30cb425
michellehuang728 <michellely.huang728@gmail.com>
2024-07-21 21:28:15 +0800

    Init commit: jump, left, right

--- ACM-metroidvania/Player.gd
+++ ACM-metroidvania/Player.gd
@@ -24,7 +24,8 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x -= SPEED
+		#velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
