17a03555d579249effbe4a5666d0b9cf5f1fa4d3
Katie <katie@Katies-MacBook-Air.local>
2024-08-20 20:41:21 -0700

    added coco's double jump and dash

--- player/player.gd
+++ player/player.gd
@@ -6,6 +6,14 @@ const SPEED = 300.0
 const STRIKE_SPEED = 0.0
 const JUMP_VELOCITY = -400.0
 
+#@export var JUMP_FORCE: int = -300
+var JUMP_COUNT = 0
+var MAX_JUMP = 2
+
+@export var DASH_SPEED = 600
+var DASHING = false
+var CAN_DASH = true
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -26,6 +34,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if is_on_floor():
+		JUMP_COUNT = 0
 		
 	if health <= 0:
 		Global.player_health = 0
@@ -51,9 +62,14 @@ func _physics_process(delta):
 					is_striking = false
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_space") and is_on_floor():
+	#if Input.is_action_just_pressed("ui_space"): #and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		#anim.play("jump")
+		
+	if Input.is_action_just_pressed("ui_space") and JUMP_COUNT < MAX_JUMP:
+		anim.play('jump')
 		velocity.y = JUMP_VELOCITY
-		anim.play("jump")
+		JUMP_COUNT += 1
 	
 	if velocity.y > 0 and anim.current_animation != "strike" and anim.current_animation != "hurt":
 		anim.play("fall")
@@ -70,7 +86,9 @@ func _physics_process(delta):
 		Global.player_direction_left = true
 	
 	if direction:
-		if is_striking and is_on_floor():
+		if DASHING and is_on_floor():
+			velocity.x = direction * DASH_SPEED
+		elif is_striking and is_on_floor():
 			velocity.x = direction * STRIKE_SPEED
 		else:
 			velocity.x = direction * SPEED
@@ -93,11 +111,22 @@ func _physics_process(delta):
 	if is_striking and anim.current_animation != "strike":
 		is_striking = false
 		
+	if Input.is_action_just_pressed("ui_shift") and CAN_DASH:
+		DASHING = true
+		CAN_DASH = false
+		$dash_duration.start()
+		$dash_again.start()
+
+
 func respawn_scene() -> void:
 	if get_tree() and anim.current_animation != "death":
 		get_tree().change_scene_to_file("res://respawn.tscn")
 		return
 	else:
 		print(":(")
-	
 
+func _on_dash_duration_timeout():
+	DASHING = false
+
+func _on_dash_again_timeout():
+	CAN_DASH = true
