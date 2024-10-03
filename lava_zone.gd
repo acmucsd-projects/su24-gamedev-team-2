781f7e6d72429071ec47f0c8ad0e57b44b6af1ad
Katie <katie@Katies-MacBook-Air-3.local>
2024-10-03 11:50:40 -0700

    lavazone background

--- lava_zone.gd
+++ lava_zone.gd
@@ -3,9 +3,6 @@ extends Node2D
 @onready var player = $player/player
 @onready var large_slime = $large_slime
 
-func _ready():
-	print(Global.num_enemies)
-
 func _on_area_2d_body_entered(body):
 	if body == player:
 		Global.player_health = 0
