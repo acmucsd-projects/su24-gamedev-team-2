724986ffb9c50b4f8e2e80b76a29abe95f6630e3
michellehuang728 <michellely.huang728@gmail.com>
2024-10-03 14:59:10 -0700

    updated final scene

--- main.gd
+++ main.gd
@@ -6,6 +6,5 @@ func _on_quit_pressed():
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://world.tscn")
 
-
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://settings.tscn")
